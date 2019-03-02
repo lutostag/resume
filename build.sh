0ee4f7c57564ad1f9971427d8dd1bb160d7f6e17
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2019-03-02 18:54:33 +0100

    cleanup of building

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-pandoc -H style.css -f markdown -t html resume.md > resume.html
-tidy -w 0 -qim resume.html || true
-weasyprint resume.html GregLutostanskiResume.pdf
+cd pdf
+yarn install
+cd ..
+
+pandoc -H style.css -f markdown -t html resume.md > build/resume.html
+tidy -w 0 -qim build/resume.html || true
+node pdf/print.js
