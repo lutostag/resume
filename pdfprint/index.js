7f795aaea073b44ab9a95e1471ca83e1202f801d
Greg Lutostanski <greg.lutostanski@mobilityhouse.com>
2019-03-09 19:32:08 +0100

    pdfpring: rename from old and include proper license

---
+++ pdfprint/index.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2017 Google Inc. All rights reserved.
+ * Copyright 2019 Greg Lutostanski, Modified from example
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const puppeteer = require('puppeteer');
+const path = require('path');
+const input = process.argv[2];
+const output = process.argv[3];
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(input, {waitUntil: 'networkidle2'});
+  await page.pdf({path: output, format: 'A4'});
+  await browser.close();
+})();
